4a37792f824a1f4b359ec02115361d07293e1bf0
Dmitrij Atjukh <dmitrij.artjukh@privatbank.ua>
2021-11-04 17:48:57 +0300

    init commit

--- src/com/pb/artjukh/hm6/Animal.java
+++ src/com/pb/artjukh/hm6/Animal.java
@@ -4,6 +4,11 @@ public class Animal {
     private String food;
     private String location;
 
+    public Animal(String food, String location) {
+        this.food = food;
+        this.location = location;
+    }
+
     public String getFood() {
         return food;
     }
@@ -26,6 +31,6 @@ public class Animal {
 
     }
     public void sleep (){
-
+        //System.out.println(name+" животное спит.");
     }
 }
