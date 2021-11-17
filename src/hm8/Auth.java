7836462370946cda89a99549d7ace2f3058c0485
Dmitrij Artjukh <dmitrij.artjukh@privatbank.ua>
2021-11-17 11:13:58 +0200

    init commit

--- src/hm8/Auth.java
+++ src/hm8/Auth.java
@@ -6,7 +6,8 @@ public class Auth {
     private String login;
     private String password;
    private  String confimPassword;
-
+   private String myloginl;
+   private  String mypassword;
     public String getLogin() {
         return login;
     }
@@ -34,12 +35,23 @@ public class Auth {
     public void signUp (String login, String password,String confimPassword)
     {
          int q = login.length() ;
-        boolean str= login.matches("^[a-zA-Z0-9]+$");
+        int w = password.length();
+         boolean str= login.matches("^[a-zA-Z0-9]+$");
+        boolean str2= password.matches("^[a-zA-Z_]+$");
 
-        if (q > 5 && q <20 && str == true) {
-            System.out.println("ok");
+        if (q > 5  && str == true && q <20) {
+            myloginl=login;
+            System.out.println("ok"+myloginl);
         } else System.out.println("исключение");
-    }
+
+
+    if (w > 5 && password.equals(confimPassword) && str2 == true) {
+        mypassword=password;
+
+        System.out.println("ok "+mypassword);
+    } else System.out.println("исключение");
+}
+
     public void signIn (String login, String password)
     {
 
