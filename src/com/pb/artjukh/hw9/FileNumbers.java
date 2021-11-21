5b012fb6943863badf27cc44d0f7706d66a84fa0
Dmitrij Artjukh <dmitrij.artjukh@privatbank.ua>
2021-11-21 18:17:32 +0200

    init commit

--- src/com/pb/artjukh/hw9/FileNumbers.java
+++ src/com/pb/artjukh/hw9/FileNumbers.java
@@ -1,7 +1,5 @@
 package com.pb.artjukh.hw9;
 
-import com.pb.artjukh.hm3.Array;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -32,6 +30,33 @@ public class FileNumbers {
     }
 
     public static void createOddNumbersFile() throws IOException {
+//        BufferedReader in = new BufferedReader(new FileReader("numbers.txt"));
+//        String s = in.readLine();
+//        String[] strScores = s.split(" ");
+////        int[] score = new int[strScores.length];
+//        for (int j = 0; j < strScores.length; j++) {
+//            score[j] = Integer.parseInt(strScores[j]);
+//        }
+        try (Scanner in = new Scanner(new File("numbers.txt")); // "try с ресурсами"
+             PrintWriter out = new PrintWriter(new FileWriter("out_file.txt"));) {
+            System.out.println("Вывод, с учетом замены целых четных чисел на '0':");
+            while (in.hasNextLine()) {
+                Scanner line = new Scanner(in.nextLine()); // Одна строка файла
+                while (line.hasNextInt()) {             // Разберем ее на числа
+                    int data = line.nextInt();          // Очередное число
+                    if (data % 2 == 0) {                // Если чётное
+                        System.out.format("%d -> 0, ", data); // Чтоб было видно, что делается
+                        data = 0;                       // Заменим его нулём
+                    } else
+                        System.out.print(data + ", ");  // Чтоб было видно, что делается
+                    out.print(data + ", ");
+                }
+                line.close();                   // Не забывать, чтобы не было утечек
+                out.println();                  // Строка закончена -- перенос строки в вых. файл
+                System.out.println();           // и на экране
+            }
+        } catch (IOException x) { System.out.println("IO error: " + x); }
+        // И всё. Файлы закрыты
     }
          public static void main (String[] args){
         try {
