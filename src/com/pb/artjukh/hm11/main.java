374c68f75fec11eed1cd6d140ce0fa323806f542
Dmitrij Artjukh <dmitrij.artjukh@privatbank.ua>
2021-12-06 18:26:51 +0300

    git push

--- src/com/pb/artjukh/hm11/main.java
+++ src/com/pb/artjukh/hm11/main.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class main {
     public static void main(String[] args) {
         ObjectMapper objectMapper=new ObjectMapper();
+
         System.out.println("1");
     }
 }
